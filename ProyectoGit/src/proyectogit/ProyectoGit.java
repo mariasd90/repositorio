00c85504d8455b82cc7bb1d0651c9fc92e86a401
maria <maria@Pili>
2017-05-27 16:57:15 +0200

    He añadido un system

--- ProyectoGit/src/proyectogit/ProyectoGit.java
+++ ProyectoGit/src/proyectogit/ProyectoGit.java
@@ -11,7 +11,7 @@ public class ProyectoGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hola");
     }
     
 }
